e813d346494953ade47782470c74492232f96492
Lifechoices <mpendulokhozamk2@gmail.com>
2021-05-18 16:37:40 +0200

    updated changes

--- test.py
+++ test.py
@@ -1,18 +1,42 @@
-# import tkinter
 import tkinter
 from tkinter import *
 from tkinter import messagebox
 
-# Creating The Application
 root = Tk()
+root.config(bg='SkyBlue')
+root.title("BMI Calculator")
 root.geometry("360x400")
-root.configure(background='skyblue')
-root.title('BMI Calculator')
-root.resizable(width=True, height=True)
+frame1 = Frame(root, highlightbackground="black", highlightcolor="green", highlightthickness=1, width=350, height=150)
+frame1.place(x=3, y=1,)
 
-bmi = 0
 
-# Defining My Buttons and Calculations
+
+# Your weight Entry
+label1 = Label(root, text='Enter Your Weight: ', bd=4, bg='#cef0f1', highlightthickness=2)
+label1.place(x=5, y=10)
+weight = Entry(root)
+weight.place(x=150, y=10)
+
+# Your Height Entry
+label2 = Label(root, text='Enter Your Height: ', bd=4, bg='#cef0f1')
+label2.place(x=5, y=50)
+height = Entry(root)
+height.place(x=150, y=50)
+
+
+gender = Label(root, text="Gender:", bd=4, bg='#cef0f1')
+gender.place(x=5, y=85)
+# Age Entry
+label4 = Label(root, text='Enter Your Age:', bd=4, bg='#cef0f1')
+label4.place(x=5, y=120)
+age = Entry(root, )
+age.place(x=150, y=120)
+
+options = ['Select...', 'Male', "Female"]
+variable = StringVar(root)
+variable.set(options[0])
+
+
 def activate(value):
     variable.set(value)
     if value != "Select...":
@@ -22,9 +46,10 @@ def activate(value):
 
 
 gender_menu = OptionMenu(root, variable, *options, command=activate)
-gender_menu.place(relx=0.3, rely=0.4)
+gender_menu.place(x=150, y=85)
 
 category = Entry(root)
+
 
 def bmi_calculator():
     try:
@@ -68,6 +93,31 @@ def bmi_calculator():
         messagebox.showerror(title=None, message='Gender was not specified or invalid entry was given')
         clear()
 
+
+# Buttons
+b1 = tkinter.Button(root, text='Calculate Your Body Mass Index', width=40, bd=3, command=bmi_calculator)
+b1.place(x=3, y=160)
+
+# Status Entry
+label6 = Label(root, text='BMI Status: ', bd=4, bg='#cef0f1')
+label6.place(x=5, y=280)
+status = Entry(root)
+status.place(x=150, y=280)
+
+
+# Body Mas Index output
+label5 = Label(root, text='Body Mass Index: ', bd=4, bg='#cef0f1')
+label5.place(x=5, y=200)
+BMI = Entry(root)
+BMI.place(x=150, y=200)
+
+# Ideal Body Mass Index
+label6 = Label(root, text='Ideal B Mass Index: ', bd=4, bg='#cef0f1')
+label6.place(x=5, y=240)
+IBMI = Entry(root)
+IBMI.place(x=150, y=240)
+
+
 def clear():
     weight.delete(0, END)
     height.delete(0, END)
@@ -77,6 +127,7 @@ def clear():
     age.delete(0, END)
     BMI.delete(0, END)
     IBMI.delete(0, END)
+    status.delete(0, END)
     age.config(state='readonly')
     BMI.config(state='readonly')
     IBMI.config(state='readonly')
@@ -84,67 +135,8 @@ def clear():
     variable.set(options[0])
 
 
-def close():
-    return root.destroy()
-
-# A frame that has all the input values
-
-
-frame1 = Frame(root, highlightbackground="black", highlightcolor="green", highlightthickness=1, width=350, height=150)
-frame1.place(x=3, y=1,)
-
-
-# Your weight Entry
-label1 = Label(root, text='Enter Your Weight: ', bd=4, bg='#cef0f1', highlightthickness=2)
-label1.place(x=5, y=10)
-weight = Entry(root)
-weight.place(x=150, y=10)
-
-# Your Height Entry
-label2 = Label(root, text='Enter Your Height: ', bd=4, bg='#cef0f1')
-label2.place(x=5, y=50)
-height = Entry(root)
-height.place(x=150, y=50)
-
-# The Gender Selection
-options = ['Select...', 'Male', "Female"]
-variable = StringVar(root)
-variable.set(options[0])
-gender = Label(root, text="Gender:", bd=4, bg='#cef0f1')
-gender.place(x=5, y=85)
-# Age Entry
-label4 = Label(root, text='Enter Your Age:', bd=4, bg='#cef0f1')
-label4.place(x=5, y=120)
-age = Entry(root, )
-age.place(x=150, y=120)
-
-# Body Mas Index output
-label5 = Label(root, text='Body Mass Index: ', bd=4, bg='#cef0f1')
-label5.place(x=5, y=200)
-BMI = Entry(root)
-BMI.place(x=150, y=200)
-
-# Status Entry
-label6 = Label(root, text='BMI Status: ', bd=4, bg='#cef0f1')
-label6.place(x=5, y=280)
-status = Entry(root)
-status.place(x=150, y=280)
-
-
-# Ideal Body Mass Index
-label6 = Label(root, text='Ideal B Mass Index: ', bd=4, bg='#cef0f1')
-label6.place(x=5, y=240)
-IBMI = Entry(root)
-IBMI.place(x=150, y=240)
-
-# Buttons
-b1 = tkinter.Button(root, text='Calculate Your Body Mass Index', width=40, bd=3, command=bmi_calculator)
-b1.place(x=3, y=160)
-
-b2 = tkinter.Button(root, text='Clear', width=10, bd=3, command=clear)
-b2.place(x=3, y=350)
-b3 = tkinter.Button(root, text='Exit', width=10, bd=3, command=close)
-b3.place(x=245, y=350)
-
-
+clear = Button(root, text='Clear', command=clear, borderwidth=5, fg="black")
+clear.place(x=3, y=350)
+quit = Button(root, text='Exit', command='exit', borderwidth=5, fg="black")
+quit.place(x=290, y=350)
 root.mainloop()
